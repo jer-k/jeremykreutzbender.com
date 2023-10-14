b3b4cf8594a92f1a7b6972230f67ca338e527b65
jer-k <jeremyk112@gmail.com>
2023-10-14 14:56:35 -0700

    Started adding components from shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import Image from 'next/image'
-
+import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
   return (
-    <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      </main>
-    </>
-
-  )
+    <div className="flex flex-col items-center justify-center space-y-2 py-4">
+      <h3>This site is under construction</h3>
+      <p>
+        I&apos;ve never used Tailwind before so bear with me as I learn how to
+        style stuff
+      </p>
+      <Progress value={10} className="w-[60%]" />
+    </div>
+  );
 }
---
+++ components/ui/header.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { SearchBar } from "@/components/ui/search-bar";
+
+export function Header() {
+  return (
+    <div className="hidden h-full flex-row md:flex">
+      <div className="container flex flex-row items-start justify-between space-y-2 p-4 sm:flex-row sm:items-center sm:space-y-0 md:h-20">
+        <div className="container flex flex-row items-start space-y-2 space-x-4">
+          <Link href="/">
+            <div className="container flex flex-row items-center space-x-2">
+              <Avatar>
+                <AvatarImage src="https://github.com/jer-k.png" alt="@jer-k" />
+                <AvatarFallback>JK</AvatarFallback>
+              </Avatar>
+              <h2>jeremykreutzbender.com</h2>
+            </div>
+          </Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/videos">Videos</Link>
+        </div>
+        <div className="container flex flex-row items-start justify-end space-y-2 space-x-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
+          <SearchBar />
+          <ModeToggle />
+        </div>
+      </div>
+    </div>
+  );
+}
