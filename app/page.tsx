037ff2a207d466771b5a53162339f3ea33a7412d
jer-k <jeremyk112@gmail.com>
2024-01-07 10:07:53 -0800

    Fix some coloring. Footer is still off

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32">
+        <section className="w-full">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl lg:text-6xl">
@@ -25,7 +25,7 @@ export default function HomePage() {
             </div>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:text-white dark:bg-slate-950">
+        <section className="w-full py-12">
           <div className="container px-4 md:px-6">
             <h2 className="text-3xl font-bold tracking-tighter text-center sm:text-4xl">
               Latest Blog Posts
