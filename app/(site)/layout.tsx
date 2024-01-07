28994adc03f17acf33265bc62b3b1b3cce0a30e6
jer-k <jeremyk112@gmail.com>
2024-01-07 15:06:46 -0800

    Move theme provider into (site) so the cv doesn't look weird

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,15 +2,25 @@ import { ReactNode } from "react";
 
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
+import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col h-screen justify-between px-16 pt-4">
-      <Header />
-      {children}
-      <Toaster />
-      <Footer />
-    </div>
+    <>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <div className="flex flex-col h-screen justify-between px-16 pt-4">
+          <Header />
+          {children}
+          <Toaster />
+          <Footer />
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from "react";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,16 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
