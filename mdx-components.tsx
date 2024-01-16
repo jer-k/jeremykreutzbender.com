15e541e5f0ff2aa558a399325a6d847b5761b60c
jer-k <jeremyk112@gmail.com>
2024-01-15 20:42:01 -0800

    Pulling over images for the posts

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,8 @@
+import Image, { ImageProps } from "next/image";
+
 import { Code } from "bright";
 import type { MDXComponents } from "mdx/types";
+
 import { PublishedOnOldBlog } from "@/components/mdx/published-on-old-blog";
 import { PublishedOnReleaseBlog } from "@/components/mdx/published-on-release-blog";
 
@@ -13,6 +16,15 @@ export const mdxComponents: MDXComponents = {
   PublishedOnOldBlog: PublishedOnOldBlog,
   PublishedOnReleaseBlog: PublishedOnReleaseBlog,
   pre: Code,
+  img: (props) => (
+    <Image
+      sizes="100vw"
+      style={{ width: "100%", height: "auto" }}
+      width={450}
+      height={450}
+      {...(props as ImageProps)}
+    />
+  ),
 };
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
