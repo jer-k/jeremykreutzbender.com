7920c6de9e8b3a88c6a8ddfdfbc14ee2d57333db
jer-k <jeremyk112@gmail.com>
2024-01-27 12:40:44 -0800

    Remove link to jer-k.github.io since it redirects now

--- components/mdx/published-on-old-blog.tsx
+++ components/mdx/published-on-old-blog.tsx
@@ -2,9 +2,7 @@ export function PublishedOnOldBlog() {
   return (
     <p className="italic">
       This post was originally published on my previous blog{" "}
-      <a href="https://jer-k.github.io" target="_blank">
-        jer-k.github.io
-      </a>
+      <a href="#">jer-k.github.io</a>
     </p>
   );
 }
