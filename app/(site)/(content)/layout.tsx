86693b3f78f41f10007fa3620768c0b4d094ee7e
jer-k <jeremyk112@gmail.com>
2024-02-04 10:55:01 -0800

    Fix layout for mobile

--- app/(site)/(content)/layout.tsx
+++ app/(site)/(content)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function BlogLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full flex justify-center">
+    <div className="md:w-full md:flex md:justify-center">
       <div className="prose dark:prose-invert">{children}</div>
     </div>
   );
