4091bdcb049159955ec47434723dae810c12552f
jer-k <jeremyk112@gmail.com>
2024-03-03 16:08:48 -0800

    Remove rehypeMdxCodeProps for now

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 import createMDX from '@next/mdx'
 import remarkFrontmatter from "remark-frontmatter"
 import remarkMdxFrontmatter from "remark-mdx-frontmatter"
-import rehypeMdxCodeProps from "rehype-mdx-code-props"
 import rehypeSlug from "rehype-slug"
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
 
@@ -14,7 +13,7 @@ const nextConfig = {
 const withMDX = createMDX({
   options: {
     remarkPlugins: [remarkFrontmatter, [remarkMdxFrontmatter, { name: "frontmatter" }]],
-    rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, rehypeMdxCodeProps],
+    rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
   }
 })
 
