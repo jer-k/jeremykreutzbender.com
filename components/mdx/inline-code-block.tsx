f57072c0bc5d15533f61d9409915955a29c5e645
jer-k <jeremyk112@gmail.com>
2024-03-15 09:00:40 -0700

    Update bright to support custom code blocks

---
+++ components/mdx/inline-code-block.tsx
@@ -0,0 +1,12 @@
+import { PropsWithChildren } from "react";
+
+export function InlineCodeBlock({ children }: PropsWithChildren) {
+  return (
+    <code
+      className="not-prose font-mono text-sm text-solaralizedlight-foreground bg-solaralizedlight
+      dark:text-solaralizeddark-foreground dark:bg-solaralizeddark rounded-md px-1.5 py-1"
+    >
+      {children}
+    </code>
+  );
+}
