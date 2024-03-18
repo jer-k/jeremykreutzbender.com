7564383ef4c8e5e4b794af3c33fb343a7b27c29d
jer-k <jeremyk112@gmail.com>
2024-03-18 09:14:48 -0700

    Fixing h2-anchors to have styling

---
+++ components/mdx/h2-with-anchor.tsx
@@ -0,0 +1,19 @@
+import { PropsWithChildren } from "react";
+
+type Props = {
+  id?: string;
+} & PropsWithChildren;
+export function H2WithAnchor({ id, children }: Props) {
+  return (
+    <h2 className="group" id={id}>
+      {children}
+      <a
+        aria-label={id}
+        href={`#${id}`}
+        className="p-2 italic font-bold opacity-0 group-hover:opacity-75"
+      >
+        #
+      </a>
+    </h2>
+  );
+}
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,6 +4,7 @@ import { Code } from "bright";
 import { Tweet } from "react-tweet";
 import type { MDXComponents } from "mdx/types";
 
+import { H2WithAnchor } from "@/components/mdx/h2-with-anchor";
 import { InlineCodeBlock } from "@/components/mdx/inline-code-block";
 import { PublishedOnOldBlog } from "@/components/mdx/published-on-old-blog";
 import { PublishedOnReleaseBlog } from "@/components/mdx/published-on-release-blog";
@@ -19,16 +20,7 @@ export const mdxComponents: MDXComponents = {
   PublishedOnReleaseBlog: PublishedOnReleaseBlog,
   pre: Code,
   code: ({ children }) => <InlineCodeBlock>{children}</InlineCodeBlock>,
-  h2: ({ id, children }) => {
-    return (
-      <h2 id={id}>
-        {children}
-        <a aria-label={id} href={`#${id}`} className="p-1 italic font-bold">
-          #
-        </a>
-      </h2>
-    );
-  },
+  h2: ({ id, children }) => <H2WithAnchor id={id}>{children}</H2WithAnchor>,
   img: (props) => (
     <Image
       sizes="100vw"
