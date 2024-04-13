c434c98acb67ef8eb90f4028a2fb2a35c3e70bd7
jer-k <jeremyk112@gmail.com>
2024-04-13 16:27:16 -0700

    Implement pagination

--- app/(site)/open_source/page.tsx
+++ app/(site)/open_source/page.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { openSourcePullRequests } from "@/lib/github";
 
 import { PullRequestCard } from "@/components/pull-request-card";
+import { Pagination } from "@/components/pagination";
 
 export const metadata: Metadata = {
   title: "Open Source",
@@ -26,8 +27,20 @@ export const metadata: Metadata = {
   },
 };
 
-export default async function OpenSourcePage() {
+type OpenSourceProps = {
+  searchParams: {
+    page?: string;
+  };
+};
+
+export default async function OpenSourcePage({
+  searchParams,
+}: OpenSourceProps) {
   const pullRequests = await openSourcePullRequests();
+
+  const page = (searchParams.page && parseInt(searchParams.page)) || 1;
+  const start = (page - 1) * 10;
+  const numPages = Math.ceil(pullRequests.length / 10);
 
   return (
     <div className="flex flex-col items-center space-y-6">
@@ -36,12 +49,13 @@ export default async function OpenSourcePage() {
       </h1>
       <div className="prose">
         <div className="flex flex-col space-y-6 not-prose">
-          {pullRequests.map((pullRequest) => (
+          {pullRequests.slice(start, start + 10).map((pullRequest) => (
             <PullRequestCard
               key={pullRequest.number}
               pullRequest={pullRequest}
             />
           ))}
+          <Pagination page={page} numPages={numPages} path="open_source" />
         </div>
       </div>
     </div>
