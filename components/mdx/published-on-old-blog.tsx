93e9b642d7794477dcb7332bf97b28414b083798
jer-k <jeremyk112@gmail.com>
2024-04-21 09:15:02 -0700

    open in a new window

--- components/mdx/published-on-old-blog.tsx
+++ components/mdx/published-on-old-blog.tsx
@@ -2,7 +2,10 @@ export function PublishedOnOldBlog() {
   return (
     <p className="italic">
       This post was originally published on my previous blog{" "}
-      <a href="https://github.com/jer-k/deprecated-jer-k.github.io">
+      <a
+        href="https://github.com/jer-k/deprecated-jer-k.github.io"
+        target="_blank"
+      >
         jer-k.github.io
       </a>
     </p>
