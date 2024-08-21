03393e9d48619e71a9b42eef4b69117ba3066745
jer-k <jeremyk112@gmail.com>
2024-08-21 11:27:46 -0700

    Updating the geist font and the OG image generator

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 import type { Metadata } from "next";
-import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
@@ -59,7 +59,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={GeistMono.variable}>
+    <html lang="en" className={GeistSans.className}>
       <body>
         {children}
         <Analytics />
