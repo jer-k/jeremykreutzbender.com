39fe30874887298146b0b97e4f2795fed25d08a6
Jeremy Kreutzbender <jeremyk112@gmail.com>
2025-11-26 15:18:33 -0800

    More storybook cleanup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,16 +3,9 @@ import type { StorybookConfig } from "@storybook/nextjs";
 import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-docs",
-  ],
+  addons: ["@storybook/addon-links"],
 
   framework: {
     name: "@storybook/nextjs",
