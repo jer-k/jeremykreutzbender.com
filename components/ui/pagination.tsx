a24984983237cc1203b44b14f7b6944c97320c33
Jeremy Kreutzbender <jeremyk112@gmail.com>
2025-11-28 10:07:22 -0800

    Fix pagination

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -66,13 +66,18 @@ function PaginationLink({
 
 function PaginationPrevious({
   className,
+  disabled = false,
   ...props
-}: React.ComponentProps<typeof PaginationLink>) {
+}: React.ComponentProps<typeof PaginationLink> & { disabled: boolean }) {
   return (
     <PaginationLink
       aria-label="Go to previous page"
       size="default"
-      className={cn("gap-1 px-2.5 sm:pl-2.5", className)}
+      className={cn(
+        "gap-1 px-2.5 sm:pl-2.5",
+        disabled && "pointer-events-none opacity-50",
+        className,
+      )}
       {...props}
     >
       <ChevronLeftIcon />
@@ -83,13 +88,18 @@ function PaginationPrevious({
 
 function PaginationNext({
   className,
+  disabled = false,
   ...props
-}: React.ComponentProps<typeof PaginationLink>) {
+}: React.ComponentProps<typeof PaginationLink> & { disabled: boolean }) {
   return (
     <PaginationLink
       aria-label="Go to next page"
       size="default"
-      className={cn("gap-1 px-2.5 sm:pr-2.5", className)}
+      className={cn(
+        "gap-1 px-2.5 sm:pr-2.5",
+        disabled && "pointer-events-none opacity-50",
+        className,
+      )}
       {...props}
     >
       <span className="hidden sm:block">Next</span>
